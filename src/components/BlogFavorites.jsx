3c0fff2f228f834f5e682e7b4e2b4225a18d9906
André Horbach <andrecomexdev@gmail.com>
2022-05-11 12:51:16 -0300

    Final version 1.01 - Blog Favorites Links correction

--- src/components/BlogFavorites.jsx
+++ src/components/BlogFavorites.jsx
@@ -19,11 +19,13 @@ function BlogFavorites(props) {
                     <div className="row row-img">
                         <div className="col-md-9">
                             <div className="blog-box-image">
+                            <a href={props.favPhotos.photo[0].id}>
                                 <img src={props.favPhotos.photo[0].url_c } alt="" data-at2x="assets/img/blog/4.jpg"></img>
+                                </a>
                             </div>
                         </div>
                         <div className="col-md-9">
-                            <h3><a href="#">{props.favPhotos.photo[0].title}</a> <i className="fas fa-angle-right"></i></h3>
+                            <h3> <a href={props.favPhotos.photo[0].id}>{props.favPhotos.photo[0].title}<i className="fas fa-angle-right"></i></a> </h3>
                         </div>
                     </div>
                 </div>
@@ -31,11 +33,13 @@ function BlogFavorites(props) {
                     <div className="row row-img">
                         <div className="col-md-9">
                             <div className="blog-box-image">
+                            <a href={props.favPhotos.photo[1].id}>
                                <img src={props.favPhotos.photo[1].url_c} alt="" data-at2x="assets/img/blog/4.jpg"></img>
+                               </a>
                             </div>
                         </div>
                         <div className="col-md-9">
-                            <h3><a href="#">{props.favPhotos.photo[1].title}</a> <i className="fas fa-angle-right"></i></h3>
+                            <h3><a href={props.favPhotos.photo[1].id}>{props.favPhotos.photo[1].title}<i className="fas fa-angle-right"></i></a></h3>
                             {/* <p><%=favPhotos.photo[1].descriptionShort%></p> */}
                         </div>
                     </div>
@@ -47,11 +51,13 @@ function BlogFavorites(props) {
                     <div className="row row-img">
                         <div className="col-md-9">
                             <div className="blog-box-image">
+                            <a href={props.favPhotos.photo[2].id}>
                                 <img src={props.favPhotos.photo[2].url_c} alt="" data-at2x="assets/img/blog/4.jpg"></img>
+                                </a>
                             </div>
                         </div>
                         <div className="col-md-9">
-                            <h3><a href="#">{props.favPhotos.photo[2].title}</a> <i className="fas fa-angle-right"></i></h3>
+                            <h3><a href={props.favPhotos.photo[2].id}>{props.favPhotos.photo[2].title} <i className="fas fa-angle-right"></i></a></h3>
                             <p>
                                 {/* <%=favPhotos.photo[2].descriptionShort%> */}
                             </p>
@@ -62,11 +68,13 @@ function BlogFavorites(props) {
                     <div className="row row-img">
                         <div className="col-md-9">
                             <div className="blog-box-image">
+                            <a href={props.favPhotos.photo[3].id}>
                                 <img src={props.favPhotos.photo[3].url_c} alt="" data-at2x="assets/img/blog/4.jpg"></img>
+                                </a>
                             </div>
                         </div>
                         <div className="col-md-9">
-                            <h3><a href="#">{props.favPhotos.photo[3].title}</a> <i className="fas fa-angle-right"></i></h3>
+                            <h3> <a href={props.favPhotos.photo[2].id}>{props.favPhotos.photo[3].title} <i className="fas fa-angle-right"></i></a></h3>
                             <p>
                                 {/* <%=favPhotos.photo[3].descriptionShort%> */}
                             </p>
